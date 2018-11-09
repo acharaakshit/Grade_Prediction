340e8a0e64b09e27d6a2b18fabb930d7a09fdad4
Akshit Achara <29024726+acharaakshit@users.noreply.github.com>
2018-11-09 17:17:44 +0530

    Add files via upload

---
+++ Heat_map.R
@@ -0,0 +1,15 @@
+data <- read.csv("/home/akshit/Documents/Downloads/gradenew.csv")
+d <- data.frame(MIDSEM = data$MIDSEM,
+                MIDSEMGRADE = data$MIDSEMGRADE,
+                MIDSEMCOL = data$MIDSEMCOL,
+                QUIZ1 = data$QUIZ1,
+                QUIZ2=data$QUIZ2,
+                PART1=data$PART1,
+                PART2 = data$PART2,
+                GRADE = data$GRADE)
+t <- cor(d) 
+
+library(corrplot)
+
+corrplot(t,type="lower",order = "hclust", t1.col = "black",t1.srt = 45)
+
