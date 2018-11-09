0112d2a2fc9405c91505727e23f8cc1e03652e0c
Akshit Achara <29024726+acharaakshit@users.noreply.github.com>
2018-11-09 19:05:24 +0530

    Add files via upload

---
+++ barplot.R
@@ -0,0 +1,3 @@
+d <- c("NBTR" = 0.4668,"NBTRpca" = 0.6972,"NBTE" = 0.62456,"NBTEpca" =0.72325  ,"DTTR" = 0.5352,"DTTRpca" = 0.6972,"DTTE"=0.6203,"DTTEpca"=0.7434,"svmTR" = 0.7385890254,"svmTRpca" = 0.6886428214,"svmTE" = 0.8333333333,"svmTEpca"=0.9025615225)
+barplot(d,main = "Accuracies of different methods",col = "blue",border = "black")
+
