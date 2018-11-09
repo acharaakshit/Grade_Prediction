c170169476c41db024b04e1ff009bf85a05e5b7b
Akshit Achara <29024726+acharaakshit@users.noreply.github.com>
2018-11-09 18:50:29 +0530

    Add files via upload

---
+++ neuralnet.R
@@ -0,0 +1,23 @@
+library(caret)
+library(neuralnet)
+
+samplesize = 0.60 * nrow(data)
+set.seed(80)
+index = sample( seq_len ( nrow ( df ) ), size = samplesize )
+
+# Create training and test set
+train = data[ index, ]
+test = data[ -index, ]
+
+max = apply(df , 2 , max)
+min = apply(df, 2 , min)
+scaled = as.data.frame(scale(df, center = min, scale = max - min))
+
+trainNN = scaled[index , ]
+testNN = scaled[-index , ]
+
+set.seed(5)
+NN = neuralnet(GRADE ~ MIDSEM + MIDSEMGRADE + MIDSEMCOL + QUIZ1 + QUIZ2 + PART1 + PART2 ,data=train,hidden=3, linear.output = T )
+plot(NN)
+dev.off()
+
